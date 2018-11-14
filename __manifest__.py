105f2d714f37885832ea16dd47dd214da90ec2f6
Nam <namk66ht@exam.com>
2018-11-14 14:49:25 +0700

    tao file

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Warehouse',
+    'description': 'Warehouse management',
+    'summary': 'Warehouse',
+    'category': 'Warehouse',
+    "sequence": 2,
+    'version': '1.0.0',
+    'author': 'Nam Nam',
+    'website': 'https://www.facebook.com/bechoibebo',
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
